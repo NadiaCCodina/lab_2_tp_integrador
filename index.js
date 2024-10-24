831557a619c124fa4df66c374189793e104a6f0e
Belén Codina <91681458+Belen-Codina@users.noreply.github.com>
2024-10-24 11:27:11 -0300

    Modulo agenda

--- index.js
+++ index.js
@@ -1,21 +1,45 @@
+// const express = require("express")
+// const app = express()
+// const routerNuevaAgenda = require("./routers/agendaRouter")
+ //const routerMostrarAgenda = require("./routers/mostrarAgendaRouter")
+
+// var path = require("path")
+// const pug= require("pug")
+// const bodyParser = require("body-parser")
+
+// //app.set('views', path.join(__dirname, 'public'));
+// app.set("view engine", "pug");
+// app.use(bodyParser.json())
+// app.use(bodyParser.urlencoded({extended:false}))
+// app.use(routerNuevaAgenda)
+// app.use(routerMostrarAgenda)
+
+// app.listen(3050, () => {
+// console.log('Server running on port 3050')
+// console.log(__dirname)
+// })
+// ////////
+
 const express = require("express")
 const app = express()
-const routerNuevaAgenda = require("./routers/agendaRouter")
-const routerMostrarAgenda = require("./routers/mostrarAgendaRouter")
-
 var path = require("path")
 const pug= require("pug")
 const bodyParser = require("body-parser")
 
+//routers
+//const routerNuevaAgenda = require("./routers/agendaRouter")
+const routerMedico= require("./routers/medicoRouter")
+const routerAgenda= require("./routers/agendaRouter")
 //app.set('views', path.join(__dirname, 'public'));
 app.set("view engine", "pug");
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:false}))
-app.use(routerNuevaAgenda)
-app.use(routerMostrarAgenda)
+//app.use(routerNuevaAgenda)
+app.use(routerMedico)
+app.use(routerAgenda)
 
 app.listen(3050, () => {
-console.log('Server running on port 3050')
+console.log('Server running on port ${3050')
 console.log(__dirname)
 })
 
